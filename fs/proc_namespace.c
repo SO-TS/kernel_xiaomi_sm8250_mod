25cbfffb9005dd06df1d391ef5095bd5024432f4
ApartTUSITU <aptusitu@outlook.com>
2026-02-10 23:13:26 +0800

    Revert "fs: Bump SuSFS to v1.5.11"
    This reverts commit 8a830056b1ce2b1df3ad99af5b909f0e56690ef2.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -21,10 +21,6 @@
 #include "pnode.h"
 #include "internal.h"
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-bool susfs_hide_sus_mnts_for_all_procs = true; // hide sus mounts for all processes by default
-#endif
-
 static __poll_t mounts_poll(struct file *file, poll_table *wait)
 {
 	struct seq_file *m = file->private_data;
@@ -110,9 +106,8 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
-	}
 #endif
 
 	if (sb->s_op->show_devname) {
@@ -152,9 +147,8 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
-	}
 #endif
 
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
@@ -222,9 +216,8 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
-	}
 #endif
 
 	/* device */
